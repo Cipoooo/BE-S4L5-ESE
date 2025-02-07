55b1a85f2c6228a0e0d930877bf9272d3977ec9d
Gabriele Cipolloni <cipolloni204@gmail.com>
2025-02-07 16:51:31 +0100

    project creation

---
+++ src/main/java/weel_project/spbt_week_project1/SpbtWeekProject1Application.java
@@ -0,0 +1,13 @@
+package weel_project.spbt_week_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtWeekProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtWeekProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/weel_project/spbt_week_project1/SpbtWeekProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package weel_project.spbt_week_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtWeekProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
